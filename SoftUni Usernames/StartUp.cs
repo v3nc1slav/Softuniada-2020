eb9f4139f8a6742b0989d0ab0ee72a1ab07b8441
v3nc1slav <v.verchov@mail.bg>
2020-10-26 22:59:15 +0200

    add new problem

---
+++ SoftUni Usernames/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SoftUni_Usernames
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse( Console.ReadLine());
+            int D = int.Parse( Console.ReadLine());
+            int L = int.Parse( Console.ReadLine());
+            int U = int.Parse( Console.ReadLine());
+            double result = 0;
+
+            if (D+L+U>N)
+            {
+                Console.WriteLine(result);
+                return;
+            }
+
+            if (D>0)
+            {
+                result = result + Math.Pow(10, D);
+            }
+
+            if (L>0)
+            {
+                result = result * Math.Pow(30, L);
+            }
+
+            if (U>0)
+            {
+                result = result * Math.Pow(30, U);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
