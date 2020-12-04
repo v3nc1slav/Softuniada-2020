fdac6a0c4a29b4e3cae2c3a2360a32aba421f20d
v3nc1slav <v.verchov@mail.bg>
2020-12-04 22:42:54 +0200

    refactoring

--- SoftUni Usernames/StartUp.cs
+++ SoftUni Usernames/StartUp.cs
@@ -1,7 +1,8 @@
 ﻿namespace SoftUni_Usernames
 {
     using System;
-    
+    using System.Numerics;
+
     class StartUp
     {
         private const int Mod = 1000000007; // prime number
@@ -13,38 +14,54 @@
             int D = int.Parse( Console.ReadLine());
             int L = int.Parse( Console.ReadLine());
             int U = int.Parse( Console.ReadLine());
-            int factN = Factorial(N);
-            int factD = Factorial(D);
-            int factL = Factorial(L);
-            int factU = Factorial(U);
+            BigInteger factN = Factorial(N);
+            BigInteger factD = Factorial(D);
+            BigInteger factL = Factorial(L);
+            BigInteger factU = Factorial(U);
+            BigInteger rowD = ModuloPower(10, D);
+            BigInteger rowL = ModuloPower(30, L);
+            BigInteger  rowU = ModuloPower(30, U);
 
             if (D<0 || L<0 || U<0)
             {
                 Console.WriteLine(0);
-                return;
             }
-            else if(D+L+U>N)
+            else if(D+L+U>N || D+L+U<U)
             {
                 Console.WriteLine(0);
             }
             else
             {
-                var result = (factN / (factD * factL * factU))
-                    * Math.Pow(10, D) % Mod
-                    * Math.Pow(30, L) % Mod
-                    * Math.Pow(30, U) % Mod;
+                var combo = rowD * rowL % Mod * rowU % Mod ;
+                var facturiel = factN / (factD * factL % Mod * factU % Mod);
+                var result = facturiel * combo % Mod;
 
                 Console.WriteLine(result);
             }
 
         }
+        private static BigInteger ModuloPower(long x, long y)
+        {
+            long result = 1;
+            while (y > 0)
+            {
+                if ((y & 1) != 0)
+                {
+                    result = (result * x) % Mod;
+                }
 
-        private static int Factorial(int f)
+                x = (x * x) % Mod;
+                y /= 2;
+            }
+
+            return result;
+        }
+        private static BigInteger Factorial(int f)
         {
             if (f == 0)
                 return 1;
             else
-                return (f * Factorial(f - 1)) % Mod;
+                return ((f * Factorial(f - 1)) %Mod);
         }
     }
 }
